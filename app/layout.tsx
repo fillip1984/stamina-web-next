7247598cb3319900e74ce53be5fdea060ae60177
fillip1984 <fillip1984@gmail.com>
2026-07-09 10:43:20 -0400

    ran format

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,15 @@
 import { Geist, Geist_Mono, Noto_Sans } from "next/font/google"
 
-import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
+import "./globals.css"
 
-const notoSansHeading = Noto_Sans({subsets:['latin'],variable:'--font-heading'});
+const notoSansHeading = Noto_Sans({
+  subsets: ["latin"],
+  variable: "--font-heading",
+})
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -22,7 +25,13 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable, notoSansHeading.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        geist.variable,
+        notoSansHeading.variable
+      )}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
